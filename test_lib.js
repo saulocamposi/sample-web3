e3e808266d887fcb85557442d0e0e59060033c4a
saulocamposi <saulocamposi@gmail.com>
2018-09-05 20:22:29 +0100

    Update -- web3 test lib

--- test_lib.js
+++ test_lib.js
@@ -1,6 +1,5 @@
 var web3 = require('web3');
-var Web3 = require('web3');
-var web3ob = new Web3();
+var web3Instance = new web3();
 
 console.log(web3);
-console.log(web3ob);
+console.log(web3Instance);
